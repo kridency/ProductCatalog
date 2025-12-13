b3d4ab4cbdb9de6c3ebfe6808d9af1c290c2e009
sergei_fatykov <sergey.fatykov@gmail.com>
2025-12-13 19:35:17 +0300

    Fix authentication principal.

--- ProductApplication/src/main/java/org/example/productcatalog/config/SecurityConfiguration.java
+++ ProductApplication/src/main/java/org/example/productcatalog/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package org.example.productcatalog.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.core.HttpHeaders;
 import org.example.productcatalog.entity.User;
 import org.example.productcatalog.exception.ApplicationException;
 import org.example.productcatalog.mapper.UserMapper;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,16 +18,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -62,19 +72,41 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain apiFilterChain(HttpSecurity http, AuthenticationManager manager) {
+    public SecurityFilterChain apiFilterChain(HttpSecurity http, AuthenticationManager manager, UserDetailsService service) {
         http.authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api-docs", "/api-docs/**", "/openapi/**", "/proxy/**",
                                 "/favicon.ico", "/error").permitAll().anyRequest().authenticated())
                 .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationManager(manager)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .securityContext(securityContext ->
                         securityContext.securityContextRepository(new HttpSessionSecurityContextRepository()))
+                .addFilterAt((request, response, filterChain) -> {
+                    HttpServletRequest httpRequest = (HttpServletRequest) request;
+                    String credentials = getCredentials(httpRequest);
+                    String login = Optional.ofNullable(credentials)
+                            .map(x -> x.substring(0, credentials.indexOf(':'))).orElse("");
+                    String password = Optional.ofNullable(credentials)
+                            .map(x -> x.substring(credentials.indexOf(':'))).orElse("");
+                    var authentication = new UsernamePasswordAuthenticationToken(login, password,
+                            service.loadUserByUsername(login).getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    filterChain.doFilter(request, response);
+                }, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    private String getCredentials(HttpServletRequest request) {
+        String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Basic ")) {
+            return new String(Base64.getDecoder().decode(headerAuth.substring(6)), StandardCharsets.UTF_8);
+        }
+        return null;
     }
 }
